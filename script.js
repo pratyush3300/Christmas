860b6e20dec6db09b7f2b03c6dc9e2ef51a4fcbf
Pratyush Prasoon <154238801+pratyush3300@users.noreply.github.com>
2024-08-30 23:13:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ window.onscroll = function () {
         nav.classList.remove('header-scrolled');
     }
 }
- 
+        // JavaScript to set carousel interval
+    document.addEventListener('DOMContentLoaded', function() {
+            var myCarousel = document.getElementById('carouselExampleInterval');
+            var carousel = new bootstrap.Carousel(myCarousel, {
+                interval: 1600
+            });
+        });
 // nav hide  
 let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
